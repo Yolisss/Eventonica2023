34a2e418689fc8fea9fb2c5892cad9ecfd086454
Yolis <yolisisabel28@gmail.com>
2023-03-14 22:47:41 -0700

    included useState to hold new value of input

--- client/src/components/eventForm.js
+++ client/src/components/eventForm.js
@@ -6,7 +6,8 @@ export default function EventForm() {
 
   const onChange = (event) => {
     //by console logging, was able to confirm we are receiving values in our console
-    console.log(event.target.value);
+    //console.log(event.target.value);
+    setInput(event.target.value);
   };
   return (
     <div>
@@ -18,5 +19,9 @@ export default function EventForm() {
   );
 }
 // onSubmit={handleSubmit}
+//in our input, we have onChange that will be called
+//within it, we have setInput that will hold the new value
+//of input. Inside our input, we have value that's holding the current
+//state of 'input'
 // value={input} onChange={ChangedValue}
 // onClick={UsersGuess}
